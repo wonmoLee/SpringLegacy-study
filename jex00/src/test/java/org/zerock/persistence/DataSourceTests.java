e6d9388a077c930dc6c15bfc01f6ae881519c820
wonmoLee <leewonmo94@gmail.com>
2020-07-25 23:02:52 +0900

    MyBatis로 Connection객체 생성 테스트

--- jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ jex00/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +26,18 @@ public class DataSourceTests {
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
 	
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+	
+	
+	
 	@Test
-	public void testConnecion() {
-		try (Connection con = dataSource.getConnection()){
+	public void testMyBatis() {
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();){
 			
+			log.info(session);
 			log.info(con);
-			
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
