f495418e115a46069ed2006699cf828fedb87d39
wonmoLee <leewonmo94@gmail.com>
2020-07-28 00:18:53 +0900

    파라미터 수집 및 리스트와 배열 처리 예제

--- jex01/src/main/java/org/zerock/controller/SampleController.java
+++ jex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,14 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -29,5 +34,39 @@ public class SampleController {
 		
 		log.info("basic get only get........");
 		
+	}
+	
+	@GetMapping("/jex01")
+	public String jex01(SampleDTO dto) {
+		
+		log.info("" + dto);
+		
+		return "jex01";
+	}
+	
+	@GetMapping("/jex02")
+	public String jex02(@RequestParam("name") String name,
+			@RequestParam("age") int age) {
+		
+		log.info("name : " + name);
+		log.info("age : " + age);
+		
+		return "jex02";
+	}
+	
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		
+		log.info("ids: " + ids);
+		
+		return "ex02List";
+	}
+	
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		
+		log.info("array ids: " + Arrays.toString(ids));
+		
+		return "ex02Array";
 	}
 }
