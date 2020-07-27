c9092faf270828cc55a5efe63adbb90ec02cd087
wonmoLee <leewonmo94@gmail.com>
2020-07-28 00:29:18 +0900

    객체 리스트 Get방식으로 받기 테스트

--- jex01/src/main/java/org/zerock/controller/SampleController.java
+++ jex01/src/main/java/org/zerock/controller/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
 
 import lombok.extern.log4j.Log4j;
 
@@ -68,5 +69,13 @@ public class SampleController {
 		log.info("array ids: " + Arrays.toString(ids));
 		
 		return "ex02Array";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
 	}
 }
