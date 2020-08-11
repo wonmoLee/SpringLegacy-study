14a178507ecf9d34bb7bf65883b71c13a459f4a2
wonmoLee <leewonmo94@gmail.com>
2020-08-11 23:43:50 +0900

    insert 처리 예시

--- jex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ jex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,8 @@ public interface BoardMapper {
 	
 	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
 }
